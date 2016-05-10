a73ec93711f477b20bfc1a9a7cc71d287c52550b
Arthur Brenaut <abrenaut@rmn.com>
2016-05-10 17:27:59 +0200

    Pypi configuration

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='mrot',
-      version='0.1',
+      version='0.3',
       description='Movie ratings over time',
       author='Arthur Brenaut',
       author_email='arthur.brenaut@gmail.com',
       packages=find_packages(),
       entry_points={
-          'console_scripts': ['mrot=mrot.mrot:main'],
+          'console_scripts': ['mrot=mrot.cli:main'],
       },
       install_requires=[
           'matplotlib',  # Plotting module
